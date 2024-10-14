bf39d8336f89c31da3da3e9da746dab7a5370da0
polamounir103 <polamounir103@gmail.com>
2024-10-15 02:40:26 +0300

    1st

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,36 @@ import "./App.css";
 import Header from "./components/shared/Header";
 import Home from "./pages/Home";
 // import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import Signin from "./pages/Signin";
 import NoPage from "./pages/NoPage";
 import AdimnDashboard from "./pages/AdimnDashboard";
 import Product from "./pages/Product";
 import MovieDetails from "./pages/MovieDetails";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/login",
-    element: <Signin />,
-  },
-  {
-    path: "/admin",
-    element: <AdimnDashboard />,
-  },
-  {
-    path: "/product/:id",
-    element: <Product />,
-  },
-  {
-    path: "/movies/:id",
-    element: <MovieDetails />,
-  },
-  {
-    path: "/*",
-    element: <NoPage />,
-  },
-]);
+
 function App() {
   return (
-    <>
-      <Header />
-      <div className="App px-5 md:px-8 lg:px-12 py-16">
-        <RouterProvider router={router} />
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <div className="px-5 md:px-8 lg:px-12 py-16">
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/login" exact element={<Signin />} />
+            <Route path="/admin" exact element={<AdimnDashboard />} />
+            <Route path="/product/:id" exact element={<Product />} />
+            <Route path="/movies/:id" exact element={<MovieDetails />} />
+            <Route path="/*" element={<NoPage />} />
+          </Routes>
+        </div>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
